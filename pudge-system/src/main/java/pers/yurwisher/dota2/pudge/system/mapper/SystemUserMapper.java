eb4c1eb08f2573d3c09d2880ef923556bfdbb27c
yq <1724537432@qq.com>
2020-10-09 18:58:09 +0800

    401异常调整

--- pudge-system/src/main/java/pers/yurwisher/dota2/pudge/system/mapper/SystemUserMapper.java
+++ pudge-system/src/main/java/pers/yurwisher/dota2/pudge/system/mapper/SystemUserMapper.java
@@ -3,6 +3,7 @@ package pers.yurwisher.dota2.pudge.system.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.apache.ibatis.annotations.Param;
 import pers.yurwisher.dota2.pudge.system.entity.SystemUser;
 import pers.yurwisher.dota2.pudge.system.pojo.qo.SystemUserQo;
 import pers.yurwisher.dota2.pudge.system.pojo.to.SystemUserTo;
@@ -22,5 +23,5 @@ public interface SystemUserMapper extends BaseMapper<SystemUser> {
      * @param qo 查询对象
      * @return 分页结果
      */
-    IPage<SystemUserTo> list(Page<SystemUserTo> page,SystemUserQo qo);
+    IPage<SystemUserTo> list(Page<SystemUserTo> page,@Param("qo") SystemUserQo qo);
 }
